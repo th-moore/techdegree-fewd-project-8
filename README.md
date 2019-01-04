5648e411802d21ac148e0dee534f8944d5e31d0b
Tom Moore <hello@thomasmoore.me>
2019-01-04 14:42:18 +0000

    Include all requirements incl. 'exceeds expectations'

--- README.md
+++ README.md
@@ -14,24 +14,24 @@ ___
 * City
 
 ### Exceeds Expectations
-1. Employees can be filtered by name or username.
+1. [**DONE**] Employees can be filtered by name or username.
 
 ___
 
 ## Modal Window
-1. Modal window displays the following details:
+1. [**DONE**] Modal window displays the following details:
 * Employee image
 * Name
 * Email
 * Cell Number
 * Detailed Address, including street name and number, city, state and post code
 * Birthdate
-2. There is a way to close the modal window.
+2. [**DONE**] There is a way to close the modal window.
 
 ### Exceeds Expectations
-1. Functionality has been added to switch back and forth between employees when the detail modal window is open.
+1. [**DONE**] Functionality has been added to switch back and forth between employees when the detail modal window is open.
 
 ___
 
 ## Matching the Mockups
-1. Directory has been styled so that all the major elements are in place and roughly matches the mockups.
+1. [**DONE**] Directory has been styled so that all the major elements are in place and roughly matches the mockups.
--- js/script.js
+++ js/script.js
@@ -2,14 +2,27 @@
 // GLOBAL VARIABLES
 /////////////////////////////////////////////////
 
-let listItems = [];
-let listContainer = document.getElementById('employeeList');
+
+// Prepare a variable that will eventually contain the array of users
+let userList = [];
+// Prepare a variable that will eventually contain each of the user card DOM elements
+// let userCards = [];
+// Prepare a variable that will eventually contain the index of the user currently viewed in the modal
+let modalIndex = 1;
+// Variable containing the number of users to be requested
+const numberOfUsers = 12;
 
 // Get the modal
-const modal = document.getElementById('myModal');
+const modal = document.getElementById("myModal");
+// Get the 'close' button in the modal
+const closeBtn = document.getElementsByClassName("m-close")[0];
+// Get the 'previous' button in the modal
+const prevBtn = document.getElementsByClassName("m-prev")[0];
+// Get the 'next' button in the modal
+const nextBtn = document.getElementsByClassName("m-next")[0];
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+// Get the search bar
+const searchBar = document.getElementById("searchbar");
 
 
 /////////////////////////////////////////////////
@@ -22,15 +35,17 @@ const span = document.getElementsByClassName("close")[0];
 function fetchData(url) {
   return fetch(url)
     .then(checkStatus)
-    .then(res => res.json())
+    .then(response => response.json())
     .catch(error => console.log('Looks like there was a problem!', error))
 }
 
-fetchData('https://randomuser.me/api/?results=12')
+fetchData(`https://randomuser.me/api/?results=${numberOfUsers}&nat=us`)
   .then(data => {
-    const userList = data.results;
+    userList = data.results;
+    console.log(userList);
     generateCards(userList);
   })
+  .then(addEventListenerToEmployees);
 
 
 // HELPER FUNCTIONS =============================
@@ -46,42 +61,94 @@ function checkStatus(response) {
 function generateCards(data) {
   const employeeListHTML = data.map(item => `
   <li class="employeeCard">
-    <img src="${item.picture.medium}">
+    <div>  
+      <img id="c-photo" src="${item.picture.medium}">
+    </div>
     <div class="card-details">
-      <h3>${item.name.first} ${item.name.last}</h3>
-      <p>${item.email}</p>
-      <p>${item.location.city}</p>
+      <h3 id="c-name">${item.name.first} ${item.name.last}</h3>
+      <p id="c-email">${item.email}</p>
+      <p id="c-city">${item.location.city}</p>
     </div>
   </li>
-  `).join('');
+  `)
+  .join('');
 
   document.getElementById('employeeList').innerHTML = employeeListHTML;
+};
+
+function setModalContent(currentIndex) {
+  document.getElementById('m-photo').src = `${userList[currentIndex].picture.medium}`;
+  document.getElementById('m-name').textContent = `${userList[currentIndex].name.first} ${userList[currentIndex].name.last}`;
+  document.getElementById('m-email').textContent = `${userList[currentIndex].email}`;
+  document.getElementById('m-mobile').textContent = `${userList[currentIndex].cell}`;
+  document.getElementById('m-address').innerHTML = `${userList[currentIndex].location.street}<br> ${userList[currentIndex].location.city}<br> ${userList[currentIndex].location.state}<br> ${userList[currentIndex].location.postcode}`;
+    let birthday = userList[currentIndex].dob.date;  
+    let bDate = birthday.substring(8,10);
+    let bMonth = birthday.substring(5,7);
+    let bYear = birthday.substring(2,4);;
+  document.getElementById('m-dob').textContent = `Birthday: ${bDate}/${bMonth}/${bYear}`;
+  document.getElementById('m-index').textContent = `Employee ${currentIndex + 1} of ${userList.length}`;
+  
+  prevBtn.style.display = "block";
+  nextBtn.style.display = "block";
+
+  if (currentIndex == numberOfUsers - 1) {
+    nextBtn.style.display = "none";
+  } else if (currentIndex == 0) {
+    prevBtn.style.display = "none";
+  }
 };
 
 
 // EVENT LISTENERS ==============================
 
-// Event listener so when cards are clicked, call the function to display the modal
-listContainer.addEventListener('click', displayModal);
+// Function to add event listeners to each of the list items as they're created.
+function addEventListenerToEmployees() {
+  let listCards = document.querySelectorAll(".employeeCard");
 
-function displayModal(e) {  
-  if (e.target.closest('.employeeCard')) {
-    let clickedCard = e.target.closest('.employeeCard');
-    console.log(clickedCard.parentNode);
-    let listIndex = clickedCard.parentNode.dataset.index;
-    console.log('Employee card has been clicked at index:' + listIndex);
-    modal.style.display = "block";
-    // setModalContent(listIndex);
-  } 
+  for (let i = 0; i < listCards.length; i += 1) {
+    listCards[i].addEventListener('click', displayModal);
+  }
 };
+
+// Event listener so when the 'next' arrow is clicked, the modal is updated with the next user's data.
+nextBtn.addEventListener('click', nextUserModal);
+
+// Event listener so when the 'previous' arrow is clicked, the modal is updated with the previous user's data.
+prevBtn.addEventListener('click', previousUserModal);
+
+// Event listener so when someone enters a term in the search bar, the list of users is filtered.
+searchBar.addEventListener('keyup', filterUsers);
 
 
 /////////////////////////////////////////////////
 // MODAL BEHAVIOURS
 /////////////////////////////////////////////////
 
+function displayModal(e) {  
+  let clickedCard = e.target.closest('li');
+  let arrayOfCards = Array.from(clickedCard.closest('ul').children);
+  modalIndex = arrayOfCards.indexOf(clickedCard);
+  
+  modal.style.display = "block";
+  prevBtn.style.display = "block";
+  nextBtn.style.display = "block";
+  
+  setModalContent(modalIndex);
+};
+
+function nextUserModal() {
+  modalIndex = modalIndex + 1;
+  setModalContent(modalIndex);
+};
+
+function previousUserModal() {
+  modalIndex = modalIndex - 1;
+  setModalContent(modalIndex);
+}
+
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeBtn.onclick = function() {
   modal.style.display = "none";
 }
 
@@ -91,3 +158,22 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+/////////////////////////////////////////////////
+// SEARCH FUNCTIONALITY
+/////////////////////////////////////////////////
+
+function filterUsers() {
+  let input = searchBar.value.toLowerCase();
+  let listCards = document.querySelectorAll(".employeeCard");
+  
+  for (let i = 0; i < listCards.length; i += 1) {
+    let name = listCards[i].lastElementChild.firstElementChild.textContent;
+    if (name.includes(input)) {
+      listCards[i].style.display = "flex";
+    } else {
+      listCards[i].style.display = "none";
+    }
+  }
+};
